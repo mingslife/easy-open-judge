8646ca3a434105ce7ce47e38b90e87119d2677d1
Ming <642203604@qq.com>
2017-12-13 14:07:06 +0800

    add some TODOs

--- application.py
+++ application.py
@@ -32,13 +32,14 @@ def submit_code():
         f.write(code)
 
     os.system('gcc -o {0}/app {0}/app.c'.format(temp_path)) # TODO 编译报错处理
-    os.system('cp {}/input.txt {}/'.format(subject_path, temp_path))
+    os.system('cp {}/input.txt {}/'.format(subject_path, temp_path)) # TODO 这里应避免用户的程序对测试用例进行操作
     os.system('cp {}/run.sh {}/'.format(subject_path, temp_path))
+    # 由于我不清楚os.system是异步进行的还是同步进行的，避免下面的代码执行出错，所有给了+1s
     time.sleep(1)
     os.system('docker run --name {0} -v {1}:/app centos bash /app/run.sh > {1}/output.txt'.format(docker_name, temp_absolute_path))
-    # TODO 超时处理，超时程序没跑完，直接docker rm -f {docker_name}
+    # TODO 超时处理，超时程序没跑完，直接docker rm -f {docker_name}强制remove掉docker容器
     time.sleep(1)
-    os.system('docker rm {}'.format(docker_name))
+    os.system('docker rm -f {}'.format(docker_name)) # TODO 这里可以做个docker容器的复用
 
     with open(subject_path + '/output.txt', 'r') as f:
         output = f.read()[:-1] # 不知道为什么后面会多一个换行
@@ -57,4 +58,3 @@ def submit_code():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
